7377c0863b2e2dd98e54766e4f13dfa52b8ac468
Vansh Aggarwal <118279381+VanshAg283@users.noreply.github.com>
2023-09-10 21:37:24 +0530

    Updated SimpleShell.c to add history file

--- SimpleShell.c
+++ SimpleShell.c
@@ -7,6 +7,7 @@
 #include <signal.h>
 
 #define MAX_LINE 100 /* The maximum length command */
+#define HISTORY_FILE "history.txt"
 
 struct Command{
     char cmd[MAX_LINE][MAX_LINE];
@@ -41,7 +42,74 @@ void add_to_history(char **cmd, pid_t pid, struct timeval start, long duration) 
     history[history_count].duration = duration;
     history_count++;
 }
-void handle_sigint(int sig) {printf("\nCtrl+C pressed. Displaying history:\n");// Display the only history that was written in the shell before running this program and not the whole history from the text filedisplay_history();exit(0);}
+
+void handle_sigint(int sig) {
+    printf("\nCtrl+C pressed. Displaying history:\n");
+    display_history();
+    exit(0);
+}
+
+void save_history_to_file() {
+    FILE *file = fopen(HISTORY_FILE, "w");
+    if (file == NULL) {
+        perror("Failed to open history file");
+        return;
+    }
+
+    for (int i = 0; i < history_count; i++) {
+        for (int j = 0; j < MAX_LINE; j++) {
+            fprintf(file, "%s ", history[i].cmd[j]);
+        }
+        fprintf(file, "\n");
+        fprintf(file, "PID: %d\n", history[i].pid);
+        fprintf(file, "Start Time: %ld seconds, %d microseconds\n", history[i].start.tv_sec, history[i].start.tv_usec);
+        fprintf(file, "Duration: %ld milliseconds\n", history[i].duration);
+        fprintf(file, "\n");
+    }
+
+    fclose(file);
+}
+
+void load_history_from_file() {
+    FILE *file = fopen(HISTORY_FILE, "r");
+    if (file == NULL) {
+        perror("Failed to open history file");
+        return;
+    }
+
+    char line[MAX_LINE];
+    int cmd_index = 0;
+
+    while (fgets(line, sizeof(line), file) != NULL) {
+        if (strcmp(line,"\n") == 0){
+            continue;
+
+        } else {
+            char first_word[MAX_LINE];
+            sscanf(line, "%s", first_word);
+
+            if (strcmp(first_word, "PID:") == 0) {
+                cmd_index = 0;
+                history[history_count].pid = 0;
+                sscanf(line, "PID: %d", &history[history_count].pid);
+            } else if (strcmp(first_word, "Start") == 0) {
+                sscanf(line, "Start Time: %ld seconds, %d microseconds", &history[history_count].start.tv_sec, &history[history_count].start.tv_usec);
+            } else if (strcmp(first_word, "Duration:") == 0) {
+                sscanf(line, "Duration: %ld milliseconds", &history[history_count].duration);
+                history_count++;
+            } else{
+                char *token;
+                token = strtok(line, " \t\n");
+                while (token != NULL) {
+                    strcpy(history[history_count].cmd[cmd_index++], token);
+                    token = strtok(NULL, " \t\n");
+                }
+            }
+        }
+    }
+
+    fclose(file);
+}
 
 void parse_command(char *cmd, char **args) {
 char *token;
@@ -58,10 +126,13 @@ int main(int argc, char *argv[]) {
     char cmd[MAX_LINE];
     char *args[MAX_LINE/2 + 1];
     int should_run = 1;
-    / Register signal handler for SIGINT
+    
+    // Register signal handler for SIGINT
     signal(SIGINT, handle_sigint);
 
-    
+    // Load history from file
+    load_history_from_file();
+
     do {
     printf("SimpleShell> ");
     fgets(cmd, MAX_LINE, stdin);
